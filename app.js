ece694b6d3da147b9fbe2ffa1da0e678862a87b1
Prayag Kushwaha <kushwahaprayag40@gmail.com>
2026-03-24 23:30:58 +0530

    Small Changes

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
@@ -36,10 +37,10 @@ app.use(express.static(path.join(__dirname, "public")));
 // });
 
 //Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings",wrapAsync( async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
-});
+}));
 
 //New Route
 app.get("/listings/new" , (req, res) => {
@@ -54,7 +55,7 @@ app.get("/listings/new" , (req, res) => {
 //     res.render("listings/show.ejs", { listing });
 // });
 
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(  async (req, res) => {
     try {
         console.log("SHOW ROUTE HIT");
 
@@ -74,36 +75,42 @@ app.get("/listings/:id", async (req, res) => {
         console.log("ERROR =", err);
         res.send(err.message);
     }
-});
+}));
 
 // Create Route
 app.post("/listings", wrapAsync(async (req, res, next) => {
+    if(!req.body.listing) {
+        throw new ExpressError(404, "Send valid data for listing");
+    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");  //  redirect ka  matlab hian ki kaam hone ke baad vapas ye page par return aa jao ✨
 }));
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(  async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-});
+}));
 
 // Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync( async (req, res) => {
+    if(!req.body.listing) {
+        throw new ExpressError(404, "Send valid data for listing");
+    }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});  // yaha 2 parameter pass kiya hain 👉(id, {...req.body.listing})
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(  async (req, res) => {
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-})
+}));
 
 // app.get("/testListing", async (req,res) => {
 //     let sampleListing = new Listing({
@@ -123,7 +130,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    let {statusCode, message} = err;
+    let {statusCode = 500, message = "Something Went Wrong"} = err;
     res.status(statusCode).send(message);
 });
 
