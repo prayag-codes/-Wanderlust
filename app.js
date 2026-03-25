5c63fc690d2e00b01a0113b5ba54825bd3568029
Prayag Kushwaha <kushwahaprayag40@gmail.com>
2026-03-25 13:12:43 +0530

    small change

--- app.js
+++ app.js
@@ -131,7 +131,8 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
     let {statusCode = 500, message = "Something Went Wrong"} = err;
-    res.status(statusCode).send(message);
+    res.render("error.ejs", {message});
+    // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
