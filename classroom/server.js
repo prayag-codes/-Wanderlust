b1428c3a24a2dacf98d180c50b536723bf3b7ff7
Prayag Kushwaha <kushwahaprayag40@gmail.com>
2026-04-15 14:33:30 +0530

    Setup the Session and the cookies related Concept

--- classroom/server.js
+++ classroom/server.js
@@ -2,33 +2,54 @@ const express = require("express");
 const app = express();
 const post = require("./routes/post.js");
 const users = require("./routes/user.js");
-const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
-app.use(cookieParser("secretcode"));
+app.use(session({secret: "mysupersecretstring" , resave: false, saveUninitialized: true}));
 
-app.get("/getsignedcookies", (req, res) => {
-    res.cookie("made-in", "India", { signed:true});
-    res.send("Sent you some signed cookies!");
-});
+app.get("/reqcounter", (req, res) => {
+    
+    if(req.session.count) {
+        req.session.count++;
+    }else {
+        req.session.count = 1;
+    }
 
-app.get("/verify", (req, res) => {
-    console.log(req.cookies);
-    res.send("Verfied the cookies, check the console!");
-});
-
-app.get("/getcookies", (req, res) => {
-    res.cookie("Greet", "Hello World");
-    res.send("Sent you some cookies!");
+    res.send(`You send a request ${req.session.count} times`);
 });
 
 
-app.get("/" , (req,res) => {
-    let {name = "anonymous"} =  req.cookies;
-    res.send(`Hello, ${name}! Welcome to our website.`);
-});
+// app.get("/test", (req, res) => {
+//     res.send("test Successful!");
+// });
 
-app.use("/users", users);
-app.use("/post", post);
+// const cookieParser = require("cookie-parser");
+
+// app.use(cookieParser("secretcode"));
+
+// app.get("/getsignedcookies", (req, res) => {
+//     res.cookie("made-in", "India", { signed:true});
+//     res.send("Sent you some signed cookies!");
+// });
+
+// app.get("/verify", (req, res) => {
+//     console.log(req.cookies);
+//     res.send("Verfied the cookies, check the console!");
+// });
+
+// app.get("/getcookies", (req, res) => {
+//     res.cookie("Greet", "Hello World");
+//     res.send("Sent you some cookies!");
+// });
+
+
+// app.get("/" , (req,res) => {
+//     let {name = "anonymous"} =  req.cookies;
+//     res.send(`Hello, ${name}! Welcome to our website.`);
+// });
+
+// app.use("/users", users);
+// app.use("/post", post);
+
 
 
 
