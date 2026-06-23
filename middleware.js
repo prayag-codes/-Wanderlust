6eca261f872005bc91be7c57f78009a03246be38
Prayag Kushwaha <kushwahaprayag40@gmail.com>
2026-06-23 10:28:57 +0530

    this is not a errorless Code

--- middleware.js
+++ middleware.js
@@ -29,16 +29,30 @@ module.exports.isOwner = async (req, res, next) => {
     next();
 };
 
-module.exports.validateListing = (req, res, next) => {
-    let {error} = listingSchema.validate(req.body);
-    if(error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError (404, errMsg);
-    } else {
-        next();
-    }
-};
+// module.exports.validateListing = (req, res, next) => {
+//     let {error} = listingSchema.validate(req.body);
+//     if(error) {
+//         let errMsg = error.details.map((el) => el.message).join(",");
+//         throw new ExpressError (404, errMsg);
+//     } else {
+//         next();
+//     }
+// };
 
+module.exports.validateListing = (req, res, next) => {
+
+    console.log("BODY =", JSON.stringify(req.body, null, 2));
+
+    let { error } = listingSchema.validate(req.body);
+
+    if(error) {
+        console.log("JOI ERROR =", error.details);
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(404, errMsg);
+    }
+
+    next();
+};
 
 module.exports.validateReview = (req, res, next) => {
     let {error} = reviewSchema.validate(req.body, {convert: true });
